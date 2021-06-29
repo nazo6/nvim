2f9ca21954b65f8cc1c2aa3f1777a1f61680b5fe
nazo6 <53177992+nazo6@users.noreply.github.com>
2021-06-29 22:07:31 +0900

    Disable rust_analyzer installer on windows

--- ts/lsp/configs/rust_analyzer.ts
+++ ts/lsp/configs/rust_analyzer.ts
@@ -4,9 +4,7 @@ import { getServerDir } from '../utils';
 
 const win = vim.fn.has('win32') !== 0;
 const unix = vim.fn.has('unix') !== 0;
-const downloadFileName = win
-  ? 'rust-analyzer-x86_64-pc-windows-msvc'
-  : unix
+const downloadFileName = unix
   ? 'rust-analyzer-x86_64-unknown-linux-gnu'
   : null;
 
@@ -17,8 +15,6 @@ export const config: ServerConfigType = {
   name,
   installer: (onExit) => {
     if (!downloadFileName) {
-      print('Not supported OS.');
-      onExit(1);
       return;
     }
     vim.fn.mkdir(cwd, 'p');
