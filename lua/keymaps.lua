6230bbd764dc5b15ea077137055e78258cffa255
nazo6 <53177992+nazo6@users.noreply.github.com>
2021-08-23 14:16:24 +0900

    Fixed keymap.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,7 +8,7 @@ map.nmap("gj", "j", map.ns)
 map.nmap("dd", [["_dd]], map.ns)
 map.map("d", [["_d"]], map.ns)
 map.vmap("c", [["_c]], map.ns)
-map.map("p", [["_p]], map.ns)
+map.vmap("p", [[_dP]], map.ns)
 
 map.map("<leader>s", [[:%s/]], map.ns)
 
