897ef529dca9713701787eeddbc20ff7c7982a0a
nazo6 <53177992+nazo6@users.noreply.github.com>
2021-12-12 00:25:49 +0900

    Color tweaks

--- lua/colors.lua
+++ lua/colors.lua
@@ -2,9 +2,30 @@ if vim.fn.has "termguicolors" == 1 then
   vim.opt.termguicolors = true
 end
 
-vim.cmd "autocmd ColorScheme * highlight MatchParen ctermbg=6 gui=bold,underline guifg=#ff9e64"
+vim.cmd [[
+]]
 
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_sidebars = { "NvimTree" }
 
-vim.cmd "colorscheme tokyonight"
+vim.cmd [[
+function! CustomHighlights() abort
+  highlight MatchParen ctermbg=6 gui=bold,underline guifg=#ff9e64
+
+  highlight TreesitterContext guibg=#3c3d37
+
+  highlight NvimTreeGitDeleted guifg=red
+  highlight NvimTreeGitRenamed guifg=yellow
+  highlight NvimTreeGitNew guifg=green
+  highlight NvimTreeGitDirty guifg=red
+	highlight NvimTreeGitStaged guifg=yellow
+	highlight NvimTreeGitMerge guifg=cyan
+endfunction
+
+augroup CustomColors
+  autocmd!
+  autocmd ColorScheme * call CustomHighlights()
+augroup END
+
+colorscheme tokyonight
+]]
