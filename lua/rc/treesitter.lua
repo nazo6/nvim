0940c39430360804a2e858aaa9b90bbe54fd38fe
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-01-08 01:54:54 +0900

    a

--- lua/rc/treesitter.lua
+++ lua/rc/treesitter.lua
@@ -1,27 +1,5 @@
 local treesitter = require "nvim-treesitter.configs"
 treesitter.setup {
-  ensure_installed = {
-    "typescript",
-    "javascript",
-    "go",
-    "rust",
-    "python",
-    "toml",
-    "yaml",
-    "regex",
-    "lua",
-    "json",
-    "jsonc",
-    "tsx",
-    "css",
-    "html",
-    "comment",
-    "c",
-    "cpp",
-    "dart",
-    "jsdoc",
-    "http",
-  },
   highlight = { enable = true },
   indent = { enable = true },
 }
