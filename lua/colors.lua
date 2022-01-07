0efe738f37a15ce801f256efc62fdd2192901992
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-01-08 01:51:22 +0900

    a

--- lua/colors.lua
+++ lua/colors.lua
@@ -11,21 +11,21 @@ local overrides = {
   NvimTreeGitStaged = { fg = "yellow" },
   NvimTreeGitMerge = { fg = "cyan" },
 }
--- Default options:
-require("kanagawa").setup {
-  undercurl = true, -- enable undercurls
-  commentStyle = "italic",
-  functionStyle = "NONE",
-  keywordStyle = "italic",
-  statementStyle = "bold",
-  typeStyle = "NONE",
-  variablebuiltinStyle = "italic",
-  specialReturn = true, -- special highlight for the return keyword
-  specialException = true, -- special highlight for exception handling keywords
-  transparent = false, -- do not set background color
-  colors = {},
-  overrides = overrides,
-}
-
--- setup must be called before loading
-vim.cmd "colorscheme kanagawa"
+local ok, kanagawa = pcall(require, "kanagawa")
+if ok then
+  kanagawa.setup {
+    undercurl = true,
+    commentStyle = "italic",
+    functionStyle = "NONE",
+    keywordStyle = "italic",
+    statementStyle = "bold",
+    typeStyle = "NONE",
+    variablebuiltinStyle = "italic",
+    specialReturn = true,
+    specialException = true,
+    transparent = false,
+    colors = {},
+    overrides = overrides,
+  }
+  vim.cmd "colorscheme kanagawa"
+end
