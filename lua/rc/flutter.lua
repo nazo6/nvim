e1b12c37f84b004cc1624d49a27a8b8ba8a46518
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-02-04 23:24:18 +0900

    update

--- lua/rc/flutter.lua
+++ lua/rc/flutter.lua
@@ -1,11 +1,16 @@
 require("flutter-tools").setup {
   ui = { border = "rounded" },
-  flutter_path = "C:\\flutter\\bin\\flutter.bat",
-  debugger = { enabled = true },
+  debugger = { enabled = true, run_via_dap = true },
   widget_guides = { enabled = true },
   closing_tags = { highlight = "ErrorMsg", prefix = ">", enabled = true },
   dev_log = { open_cmd = "tabedit" },
   dev_tools = { autostart = true, autoopen_browser = true },
   outline = { open_cmd = "30vnew" },
-  lsp = { settings = { showTodos = true, completeFunctionCalls = true } },
+  lsp = {
+    vim.tbl_extend("force", {
+      require("rc.lsp.utils").create_config {},
+    }, {
+      settings = { showTodos = true, completeFunctionCalls = true },
+    }),
+  },
 }
