4cef07df943fe0cacf5b880e975e9b7a98080f35
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-02-05 10:08:05 +0900

    Fix flutter settings

--- lua/rc/flutter.lua
+++ lua/rc/flutter.lua
@@ -6,11 +6,7 @@ require("flutter-tools").setup {
   dev_log = { open_cmd = "tabedit" },
   dev_tools = { autostart = true, autoopen_browser = true },
   outline = { open_cmd = "30vnew" },
-  lsp = {
-    vim.tbl_extend("force", {
-      require("rc.lsp.utils").create_config {},
-    }, {
-      settings = { showTodos = true, completeFunctionCalls = true },
-    }),
-  },
+  lsp = vim.tbl_extend("force", require("rc.lsp.utils").create_config {}, {
+    settings = { showTodos = true, completeFunctionCalls = true },
+  }),
 }
