235d1ab58da5d5e3523f547b37d8129f53779eb5
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-02-06 17:21:35 +0900

    Fix rust-analyzer setting

--- lua/rc/lsp/configs/rust_analyzer.lua
+++ lua/rc/lsp/configs/rust_analyzer.lua
@@ -1,5 +1,7 @@
+local create_config = require("rc.lsp.utils").create_config
+
 return function(server)
-  local config = {}
+  local config = create_config {}
 
   require("rust-tools").setup {
     server = vim.tbl_deep_extend("force", server:get_default_options(), config),
