33fad76618cff8049c150b2dc16dc6afabbdbe25
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-03-19 22:18:19 +0900

    workaround

--- lua/config/early-settings.lua
+++ lua/config/early-settings.lua
@@ -11,6 +11,14 @@ else
   require "config.global"
 end
 
+-- workaraound for https://github.com/neovim/neovim/issues/17762
+if require("config.utils").os == "Windows_NT" then
+  os.execute = function(cmd)
+    vim.fn.system(cmd)
+    return vim.v.shell_error
+  end
+end
+
 vim.g.do_filetype_lua = 1
 vim.g.did_load_filetypes = 0
 vim.g.loaded_matchparen = 1
