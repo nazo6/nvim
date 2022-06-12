a4c10c461d2d23105e4ba5edf3f80ad5f63c5b08
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-06-12 13:55:17 +0900

    Don't notify "No information available" on hover

--- lua/config/plugin/lsp/configs/common.lua
+++ lua/config/plugin/lsp/configs/common.lua
@@ -35,6 +35,19 @@ return {
   end,
   capabilities = capabilities,
   handlers = {
-    ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+    ["textDocument/hover"] = function(_, result, ctx, config)
+      local util = require "vim.lsp.util"
+      config = config or {}
+      config.focus_id = ctx.method
+      if not (result and result.contents) then
+        return
+      end
+      local markdown_lines = util.convert_input_to_markdown_lines(result.contents)
+      markdown_lines = util.trim_empty_lines(markdown_lines)
+      if vim.tbl_isempty(markdown_lines) then
+        return
+      end
+      return util.open_floating_preview(markdown_lines, "markdown", { border = "rounded" })
+    end,
   },
 }
