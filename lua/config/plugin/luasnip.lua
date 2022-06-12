3d275b4ffc51d8627a9f479d25db89b2a2b6cf36
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-06-12 19:25:55 +0900

    Improve tab map

--- lua/config/plugin/luasnip.lua
+++ lua/config/plugin/luasnip.lua
@@ -1,12 +1,7 @@
 local luasnip = require "luasnip"
 
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
 luasnip.config.set_config {
   history = true,
-  -- Update more often, :h events for more info.
   updateevents = "TextChanged,TextChangedI",
 }
 
--- lua/config/plugin/tabout.lua
+++ lua/config/plugin/tabout.lua
@@ -5,17 +5,17 @@ require("tabout").setup {
   backwards_tabkey = "",
 }
 
-local luasnip = require "luasnip"
-
 vim.keymap.set("i", "<Tab>", function()
-  if luasnip.expand_or_jumpable() then
+  local ok, luasnip = pcall(require, "luasnip")
+  if ok and luasnip.expand_or_jumpable() then
     send_key "<Plug>luasnip-expand-or-jump"
   else
     send_key "<Plug>(Tabout)"
   end
 end)
 vim.keymap.set("i", "<S-Tab>", function()
-  if luasnip.jumpable(-1) then
+  local ok, luasnip = pcall(require, "luasnip")
+  if ok and luasnip.jumpable(-1) then
     send_key "<Plug>luasnip-jump-prev"
   else
     send_key "<Plug>(TaboutBack)"
