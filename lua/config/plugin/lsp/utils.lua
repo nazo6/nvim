c37da1957d495d46f70a7c0bebd3bf0f3c5ffa6a
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-07-20 12:54:47 +0900

    Revert "Revert "Add mason.nvim""
    This reverts commit 9386ee7a0eca7ecef2ecfe58cc69b2569e3781af.

--- lua/config/plugin/lsp/utils.lua
+++ lua/config/plugin/lsp/utils.lua
@@ -11,11 +11,9 @@ M.create_config = function(server_config)
 end
 
 M.create_setup = function(server_config)
-  return function(server)
+  return function(server_name)
     local lspconfig = require "lspconfig"
-    lspconfig[server.name].setup(M.create_config(server_config))
-
-    server:attach_buffers()
+    lspconfig[server_name].setup(M.create_config(server_config))
   end
 end
 
