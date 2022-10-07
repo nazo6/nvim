88edce93d58ce178ef1628bf09223595b6528540
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-10-07 12:45:47 +0900

    Remove which-key

--- lua/user/plugins/tools.lua
+++ lua/user/plugins/tools.lua
@@ -55,17 +55,6 @@ return function(use)
   }
 
   use {
-    "folke/which-key.nvim",
-    config = function()
-      require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end,
-  }
-
-  use {
     "folke/trouble.nvim",
     requires = { { "kyazdani42/nvim-web-devicons", module = "nvim-web-devicons" } },
     cmd = "Trouble*",
