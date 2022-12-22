372559df1ebeaa6f6c0a13d8041ff279456f2475
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-12-22 18:51:12 +0900

    fix

--- lua/user/config/toggleterm.lua
+++ lua/user/config/toggleterm.lua
@@ -12,7 +12,7 @@ end
 
 return {
   setup = function()
-    vim.keymap.set("n", [[<c-\>]], '<cmd>execute v:count . "ToggleTerm"<cr>', { desc = "[toggleterm] toggle" })
+    vim.keymap.set("n", [[<c-\>]], '<cmd>execute v:count1 . "ToggleTerm"<cr>', { desc = "[toggleterm] toggle" })
     vim.keymap.set("t", [[<c-\>]], "<cmd>ToggleTerm<cr>", { desc = "[toggleterm] toggle" })
     vim.keymap.set("t", "<ESC>", "<C-\\><C-n>")
 
