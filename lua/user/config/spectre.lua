c6941b7bcf89d9373c1d31f3b53b0219f1c436f2
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-12-22 14:09:12 +0900

    update

--- lua/user/config/spectre.lua
+++ lua/user/config/spectre.lua
@@ -3,6 +3,10 @@ return {
     vim.keymap.set("n", "<leader>S", "<cmd>lua require('spectre').open()<CR>", { noremap = true, silent = true })
   end,
   config = function()
-    require("spectre").setup {}
+    require("spectre").setup {
+      line_sep_start = "┌──────────────────────────────────────",
+      result_padding = "│ ",
+      line_sep = "└──────────────────────────────────────",
+    }
   end,
 }
