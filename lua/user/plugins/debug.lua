a9fb6da9aad4d04f68e8e2c0a3ae55926994a4a6
nazo6 <53177992+nazo6@users.noreply.github.com>
2023-01-30 22:07:35 +0900

    naze

--- lua/user/plugins/debug.lua
+++ lua/user/plugins/debug.lua
@@ -1,10 +1,13 @@
 return {
+  -- ↓なんかdapをいれると最初のファイルでfiletypeがdetectされない
+  --[[
   {
     "mfussenegger/nvim-dap",
     config = function()
       require "user.config.dap"
     end,
   },
+  ]]
   {
     "rcarriga/nvim-dap-ui",
   },
