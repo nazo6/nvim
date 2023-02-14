9ae7b67a4a655d4620af1a87d9e67f09f6cf4f47
nazo6 <53177992+nazo6@users.noreply.github.com>
2023-02-14 20:46:09 +0900

    minor commit

--- lua/user/config/neo-tree.lua
+++ lua/user/config/neo-tree.lua
@@ -64,7 +64,7 @@ return {
       },
       window = {
         position = "left",
-        width = 40,
+        width = 30,
         mapping_options = {
           noremap = true,
           nowait = true,
