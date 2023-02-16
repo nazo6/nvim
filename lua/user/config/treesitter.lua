3d5f98a9014a83c4c0f5226f9aed59f6dbdcd970
nazo6 <53177992+nazo6@users.noreply.github.com>
2023-02-17 00:56:20 +0900

    Some treesitter configs

--- lua/user/config/treesitter.lua
+++ lua/user/config/treesitter.lua
@@ -55,6 +55,24 @@ treesitter.setup {
     enable = true,
   },
   autotag = { enable = true },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25,
+    persist_queries = false,
+    keybindings = {
+      toggle_query_editor = "o",
+      toggle_hl_groups = "i",
+      toggle_injected_languages = "t",
+      toggle_anonymous_nodes = "a",
+      toggle_language_display = "I",
+      focus_language = "f",
+      unfocus_language = "F",
+      update = "R",
+      goto_node = "<cr>",
+      show_help = "?",
+    },
+  },
 }
 
 require("treesitter-context").setup {
