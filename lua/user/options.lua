fd234c180f954895a73af1a445af2bef2b5e6060
nazo6 <53177992+nazo6@users.noreply.github.com>
2023-02-19 01:45:06 +0900

    Add lsp signs

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -75,3 +75,8 @@ opt.pumblend = 15
 vim.diagnostic.config {
   virtual_lines = false,
 }
+
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
