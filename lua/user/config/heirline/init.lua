430a1969f118f085f5fbaafc2270c8d04c4a1374
nazo6 <53177992+nazo6@users.noreply.github.com>
2023-02-25 11:25:04 +0900

    Don't use winbar on floating

--- lua/user/config/heirline/init.lua
+++ lua/user/config/heirline/init.lua
@@ -4,3 +4,6 @@ require("heirline").setup {
   statusline = utils.lrequire "statusline",
   winbar = utils.lrequire "winbar",
 }
+
+vim.api.nvim_del_augroup_by_name "Heirline_init_winbar"
+vim.o.winbar = "%{%v:lua.require'heirline'.eval_winbar()%}"
