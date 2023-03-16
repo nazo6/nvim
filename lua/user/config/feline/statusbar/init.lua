8f032036d6a062e60a74b422051754f974881463
nazo6 <53177992+nazo6@users.noreply.github.com>
2023-03-16 22:23:29 +0900

    Remove feline config

--- lua/user/config/feline/statusbar/init.lua
+++
@@ -1,4 +0,0 @@
-return {
-  default = require "user.config.feline.statusbar.default",
-  explorer = require "user.config.feline.statusbar.explorer",
-}
--- lua/user/config/feline/winbar/init.lua
+++
@@ -1,4 +0,0 @@
-return {
-  default = require "user.config.feline.winbar.default",
-  explorer = require "user.config.feline.winbar.explorer",
-}
