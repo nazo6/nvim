587b0c291c1b80ef3888302d9a104c93720eacc4
nazo6 <53177992+nazo6@users.noreply.github.com>
2023-03-16 22:04:41 +0900

    Add neoconf

--- lua/user/config/lsp/configs/lua_ls.lua
+++ lua/user/config/lsp/configs/lua_ls.lua
@@ -1,6 +1,10 @@
 local create_setup = require("user.config.lsp.utils").create_setup
 
-require("neodev").setup {}
+require("neodev").setup {
+  library = {
+    enabled = false,
+  },
+}
 
 return create_setup {
   settings = {
