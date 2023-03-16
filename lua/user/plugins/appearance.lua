19a650db49e8542b787518c90709c2304931180d
nazo6 <53177992+nazo6@users.noreply.github.com>
2023-03-16 21:01:51 +0900

    Fix statuscolumn

--- lua/user/plugins/appearance.lua
+++ lua/user/plugins/appearance.lua
@@ -70,9 +70,14 @@ return {
       {
         "luukvbaal/statuscol.nvim",
         config = function()
+          local builtin = require "statuscol.builtin"
           require("statuscol").setup {
-            foldfunc = "builtin",
-            setopt = true,
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
           }
         end,
       },
