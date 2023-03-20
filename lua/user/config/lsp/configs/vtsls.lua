bf21974a04f76d9aeff0c1e33a98b6ed3554ff10
nazo6 <53177992+nazo6@users.noreply.github.com>
2023-03-20 21:20:23 +0900

    Update vtsls config

--- lua/user/config/lsp/configs/vtsls.lua
+++ lua/user/config/lsp/configs/vtsls.lua
@@ -1,9 +1,18 @@
-local create_setup = require("user.config.lsp.utils").create_setup
+local create_config = require("user.config.lsp.utils").create_config
+local root_pattern = require("lspconfig").util.root_pattern
+
+local node_root_pattern = root_pattern("package.json", "tsconfig.json")
 
 require "vtsls"
 
-return create_setup {
-  on_attach = function(client)
-    client.server_capabilities.documentFormattingProvider = false
-  end,
-}
+return function(server_name)
+  local config = create_config {
+    root_dir = node_root_pattern,
+    on_attach = function(client)
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
+    end,
+    single_file_support = false,
+  }
+  require("lspconfig").vtsls.setup(config)
+end
