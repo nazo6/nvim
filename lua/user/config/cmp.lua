98c2d7249525e765f74a555fba11ea371fa3b520
nazo6 <git@nazo6.dev>
2023-03-26 21:07:04 +0900

    Show tailwindcss colors on cmp

--- lua/user/config/cmp.lua
+++ lua/user/config/cmp.lua
@@ -34,15 +34,24 @@ cmp.setup {
   },
   formatting = {
     format = require("lspkind").cmp_format {
-      with_text = false,
-      menu = {
-        nvim_lsp = "[LSP]",
-        luasnip = "[snippet]",
-        buffer = "[buffer]",
-        copilot = "[copilot]",
-        calc = "[calc]",
-        path = "[path]",
-      },
+      before = function(entry, vim_item)
+        if vim_item.kind == "Color" and entry.completion_item.documentation then
+          local _, _, r, g, b = string.find(entry.completion_item.documentation, "^rgb%((%d+), (%d+), (%d+)")
+          if r then
+            local color = string.format("%02x", r) .. string.format("%02x", g) .. string.format("%02x", b)
+            local group = "Tw_" .. color
+            if vim.fn.hlID(group) < 1 then
+              vim.api.nvim_set_hl(0, group, { fg = "#" .. color })
+            end
+            vim_item.kind = "■"
+            vim_item.kind_hl_group = group
+            return vim_item
+          end
+        end
+        vim_item.kind = require("lspkind").symbolic(vim_item.kind) and require("lspkind").symbolic(vim_item.kind)
+          or vim_item.kind
+        return vim_item
+      end,
     },
     fields = { "kind", "abbr", "menu" },
   },
