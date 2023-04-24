2745466d0653aed4a998100f2efec5dd0e24135b
nazo6 <git@nazo6.dev>
2023-04-25 01:06:45 +0900

    Better fvim restart

--- lua/user/ginit.lua
+++ lua/user/ginit.lua
@@ -1,22 +1,22 @@
-vim.opt.guifont = "PlemolJPConsole NF:h12"
-
 local restart_cmd = nil
 
 local load_restart_init_cmd = [=["lua require[[user.restart_init]]"]=]
 
 if vim.g.neovide then
+  vim.opt.guifont = "PlemolJPConsole NF:h12"
   if vim.fn.has "wsl" == 1 then
     restart_cmd = "silent! !nohup neovide.exe --wsl -- -c '" .. load_restart_init_cmd .. "' &"
   else
     restart_cmd = "silent! !neovide.exe -- -c " .. load_restart_init_cmd
   end
 elseif vim.g.fvim_loaded then
-  vim.opt.guifont = "PlemolJPConsole NF:h15"
+  vim.opt.guifont = "PlemolJP Console:h15"
 
   if vim.fn.has "wsl" == 1 then
     restart_cmd = "silent! !nohup fvim.exe --wsl -c '" .. load_restart_init_cmd .. "' &"
   else
-    restart_cmd = "fvim.exe -c " .. load_restart_init_cmd
+    restart_cmd =
+      [=[silent! !powershell -Command "Start-Process -FilePath fvim.exe -ArgumentList '-c','\"lua require[[user.restart_init]]\"' ]=]
   end
 end
 
@@ -29,5 +29,6 @@ vim.api.nvim_create_user_command("Restart", function()
 
   require("possession.session").save("restart", { no_confirm = true })
   vim.cmd [[silent! bufdo bwipeout]]
-  vim.fn.jobstart(restart_cmd, { detach = true })
+  vim.cmd(restart_cmd)
+  vim.cmd [[qa!]]
 end, {})
