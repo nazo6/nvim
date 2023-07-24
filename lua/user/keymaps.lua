e729cd4e768cca8fbdcf0a5c19af0c5619fe87c2
nazo6 <git@nazo6.dev>
2023-07-24 11:05:23 +0900

    improve terminal

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -66,3 +66,40 @@ map("n", "<leader>r", function()
     input:unmount()
   end)
 end)
+
+map("t", "<C-p>", function()
+  local ins_mode_pos_res = vim.fn.getcursorcharpos(0)
+  vim.notify(vim.inspect(ins_mode_pos_res))
+end)
+
+-- inspired by term-edit.nvim, simple version
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    map("n", "i", function()
+      local term_mode_pos_res = vim.fn.getcursorcharpos(0)
+      local term_mode_pos = { term_mode_pos_res[2], term_mode_pos_res[3] }
+
+      vim.cmd.startinsert()
+
+      vim.defer_fn(function()
+        local ins_mode_pos_res = vim.fn.getcursorcharpos(0)
+        local ins_mode_pos = { ins_mode_pos_res[2], ins_mode_pos_res[3] }
+
+        if term_mode_pos[1] == ins_mode_pos[1] then
+          local diff = term_mode_pos[2] - ins_mode_pos[2]
+          if diff < 0 then
+            local keys = string.rep("<Left>", diff * -1)
+            local k = vim.api.nvim_replace_termcodes(keys, true, true, true)
+            vim.api.nvim_feedkeys(k, "n", false)
+          end
+          if diff > 0 then
+            local keys = string.rep("<Right>", diff)
+            local k = vim.api.nvim_replace_termcodes(keys, true, true, true)
+            vim.api.nvim_feedkeys(k, "n", false)
+          end
+        end
+      end, 10)
+    end)
+  end,
+})
