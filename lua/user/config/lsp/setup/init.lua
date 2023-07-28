978356c1411a8029f59438e19603996a9471bf5d
nazo6 <git@nazo6.dev>
2023-07-28 16:10:34 +0900

    update

--- lua/user/config/lsp/setup/init.lua
+++ lua/user/config/lsp/setup/init.lua
@@ -1,15 +1,23 @@
 local server_configs = require "user.config.lsp.setup.server-configs"
 local default_setup = require("user.config.lsp.setup.config-builder").default_setup
 
-local servers = require("mason-lspconfig").get_installed_servers()
-vim.list_extend(servers, { "satysfi" })
-
-for _, server_name in ipairs(servers) do
+local function setup(server_name)
   local config_fn = server_configs[server_name]
   if config_fn == nil then
     config_fn = default_setup
   end
   config_fn(server_name)
+end
+
+require("mason-lspconfig").setup_handlers {
+  function(server_name)
+    setup(server_name)
+  end,
+}
+
+local local_servers = { "satysfi" }
+for _, server_name in ipairs(local_servers) do
+  setup(server_name)
 end
 
 require "user.config.lsp.setup.attach"
