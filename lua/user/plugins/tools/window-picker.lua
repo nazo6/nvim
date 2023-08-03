919009329ec291f3ab7f48b3ff818542dbd9a2a8
nazo6 <git@nazo6.dev>
2023-08-03 20:18:40 +0900

    Add nvim-ts-context-commentstring

--- lua/user/plugins/tools/window-picker.lua
+++ lua/user/plugins/tools/window-picker.lua
@@ -3,14 +3,12 @@ return {
   config = function()
     require("window-picker").setup {
       autoselect_one = true,
-      include_current = false,
       filter_rules = {
         bo = {
           filetype = { "NvimTree", "neo-tree", "neo-tree-popup", "notify" },
           buftype = { "terminal", "quickfix" },
         },
       },
-      other_win_hl_color = "#e35e4f",
     }
   end,
 }
