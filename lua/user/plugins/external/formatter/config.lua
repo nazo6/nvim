0dc7f5c63767c2caa5b74eaf56cb163c4670f3ea
nazo6 <git@nazo6.dev>
2023-08-17 20:30:14 +0900

    Use prettier for markdown

--- lua/user/plugins/external/formatter/config.lua
+++ lua/user/plugins/external/formatter/config.lua
@@ -1,40 +1,50 @@
-local utils = require"user.plugins.external.formatter.utils"
+local utils = require "user.plugins.external.formatter.utils"
 
 local stylua = {
   name = "stylua",
   formatter = require("formatter.filetypes.lua").stylua,
   condition = function()
-    return vim.fn.executable("stylua") == 1
+    return vim.fn.executable "stylua" == 1
   end,
-  filetypes = { "lua" }
+  filetypes = { "lua" },
 }
 
-local prettierd_pat = require"lspconfig.util".root_pattern(".prettierrc", ".prettierrc.js", ".prettierrc.cjs")
+local prettierd_pat = require("lspconfig.util").root_pattern(".prettierrc", ".prettierrc.js", ".prettierrc.cjs")
 local prettierd = {
   name = "prettierd",
   formatter = require "formatter.defaults.prettierd",
   condition = function()
-    return (utils.executable("prettierd") and prettierd_pat(vim.fn.expand('%')) ~= nil)
+    return (utils.executable "prettierd" and prettierd_pat(vim.fn.expand "%") ~= nil)
   end,
   filetypes = {
     "javascript",
     "javascriptreact",
     "json",
     "jsonc",
-    "markdown",
     "typescript",
     "typescriptreact",
   },
 }
 
-local denofmt_ignore_pat = require"lspconfig.util".root_pattern("deno.json", "deno.jsonc")
+local prettierd_md = {
+  name = "prettierd",
+  formatter = require "formatter.defaults.prettierd",
+  condition = function()
+    return true
+  end,
+  filetypes = {
+    "markdown",
+  },
+}
+
+local denofmt_ignore_pat = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
 local denofmt = {
   name = "denofmt",
   formatter = require "formatter.defaults.denofmt",
   condition = function()
     if prettierd.condition() then
       return false
-    elseif utils.executable("deno") and not(denofmt_ignore_pat(vim.fn.expand('%'))) then
+    elseif utils.executable "deno" and not (denofmt_ignore_pat(vim.fn.expand "%")) then
       return true
     else
       return false
@@ -45,12 +55,14 @@ local denofmt = {
     "javascriptreact",
     "json",
     "jsonc",
-    "markdown",
     "typescript",
     "typescriptreact",
   },
 }
 
 return {
-  stylua, prettierd, denofmt
+  stylua,
+  prettierd,
+  denofmt,
+  prettierd_md,
 }
