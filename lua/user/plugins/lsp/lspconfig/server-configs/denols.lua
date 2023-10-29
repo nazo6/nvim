d6996a09971c015189ddc7fe127408dc341f8575
nazo6 <git@nazo6.dev>
2023-10-29 22:23:50 +0900

    Improve deno vtsls detect

--- lua/user/plugins/lsp/lspconfig/server-configs/denols.lua
+++ lua/user/plugins/lsp/lspconfig/server-configs/denols.lua
@@ -1,30 +1,10 @@
 local create_setup = require("user.plugins.lsp.lspconfig.config-builder").create_setup
 local root_pattern = require("lspconfig").util.root_pattern
 
-local node_root_pattern = root_pattern("package.json", "tsconfig.json")
-local git_root_pattern = root_pattern ".git"
-local deno_root_pattern = root_pattern("deno.json", "deno.jsonc")
-
-local function deno_root(path)
-  local deno_root_path = deno_root_pattern(path)
-  if deno_root_path then
-    return deno_root_path
-  end
-
-  if node_root_pattern(path) then
-    return nil
-  end
-
-  local git_root = git_root_pattern(path)
-  if git_root then
-    return git_root
-  else
-    return vim.fn.fnamemodify(path, ":h")
-  end
-end
+local node_or_deno = require("user.plugins.lsp.lspconfig.server-configs.ts_util").node_or_deno
 
 return create_setup {
-  root_dir = deno_root,
+  root_dir = node_or_deno "deno",
   settings = {
     enable = true,
     lint = true,
--- lua/user/plugins/lsp/lspconfig/server-configs/vtsls.lua
+++ lua/user/plugins/lsp/lspconfig/server-configs/vtsls.lua
@@ -1,11 +1,10 @@
 local create_setup = require("user.plugins.lsp.lspconfig.config-builder").create_setup
+local node_or_deno = require("user.plugins.lsp.lspconfig.server-configs.ts_util").node_or_deno
 local root_pattern = require("lspconfig").util.root_pattern
-
-local node_root_pattern = root_pattern("package.json", "tsconfig.json")
 
 require "vtsls"
 
 return create_setup {
-  root_dir = node_root_pattern,
+  root_dir = node_or_deno "node",
   single_file_support = false,
 }
