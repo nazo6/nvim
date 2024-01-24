26b40dffdf5b6ff9b222adf618a8f84e15a1bf77
nazo6 <git@nazo6.dev>
2024-01-24 14:57:40 +0900

    Remove clipboard config

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -8,20 +8,6 @@ opt.mousemodel = "popup"
 vim.cmd.aunmenu { "PopUp.How-to\\ disable\\ mouse" }
 vim.cmd.aunmenu { "PopUp.-1-" }
 
-if vim.fn.has "wsl" == 1 or require("user.utils").is_win then
-  vim.g.clipboard = {
-    name = "win32yank",
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
-  }
-end
 opt.clipboard:append "unnamedplus"
 
 opt.undofile = true
