0361a056307885963e698027110d279bd6dd07cf
nazo6 <git@nazo6.dev>
2024-03-23 01:04:21 +0900

    Add skkeleton

---
+++ lua/user/plugins/tools/skkleton.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "vim-skk/skkeleton",
+    keys = { { "<C-j>", mode = { "i", "c" }, "<Plug>(skkeleton-enable)" } },
+    dependencies = { "vim-denops/denops.vim", "delphinus/skkeleton_indicator.nvim" },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "skkeleton-initialize-pre",
+        callback = function()
+          vim.fn["skkeleton#config"] {
+            eggLikeNewline = true,
+            registerConvertResult = true,
+            sources = { "skk_server" },
+            skkServerReqEnc = "utf-8",
+            skkServerResEnc = "utf-8",
+          }
+        end,
+      })
+    end,
+    config = function()
+      require("skkeleton_indicator").setup {}
+    end,
+  },
+}
