23fd4ef7f69a5b01489c10e5bf27c955780f53d6
nazo6 <git@nazo6.dev>
2024-05-31 01:09:30 +0900

    Add oil

---
+++ lua/user/plugins/tools/oil.lua
@@ -0,0 +1,24 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {
+    use_default_keymaps = false,
+    keymaps = {
+      ["?"] = "actions.show_help",
+      ["<CR>"] = "actions.select",
+      ["<C-s>"] = "actions.select_vsplit",
+      ["<C-t>"] = "actions.select_tab",
+      ["<C-p>"] = "actions.preview",
+      ["<C-c>"] = "actions.close",
+      ["<BS>"] = "actions.parent",
+      ["_"] = "actions.open_cwd",
+      ["`"] = "actions.cd",
+      ["~"] = "actions.tcd",
+      ["gs"] = "actions.change_sort",
+      ["gx"] = "actions.open_external",
+      ["g."] = "actions.toggle_hidden",
+      ["g\\"] = "actions.toggle_trash",
+    },
+  },
+  cmd = "Oil",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+}
