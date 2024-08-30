8c50d24393479e6bc49c0f56d113a9d8495a68f6
nazo6 <git@nazo6.dev>
2024-08-30 12:58:26 +0900

    replace spectre with grug-far

--- lua/user/early-init.lua
+++ lua/user/early-init.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 
 --- speeded up map function
 ---@param mode string|table
---
+++ lua/user/plugins/tools/grug-far.lua
@@ -0,0 +1,6 @@
+return {
+  "MagicDuck/grug-far.nvim",
+  keys = { { mode = { "n" }, "<leader>S", "<cmd>grugFar<CR>" } },
+  cmd = { "grugFar" },
+  opts = {},
+}
