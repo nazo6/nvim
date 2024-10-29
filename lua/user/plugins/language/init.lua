c4fccd7dd455b5768378135c8c369a33b386c1f3
nazo6 <git@nazo6.dev>
2024-10-29 23:15:07 +0900

    update tinymist config

--- lua/user/plugins/language/init.lua
+++ lua/user/plugins/language/init.lua
@@ -94,6 +94,16 @@ return Args.feature.vscode and {}
       build = function()
         require("typst-preview").update()
       end,
+      opts = {
+        get_root = function(fname)
+          local util = require "lspconfig.util"
+          local root = util.find_git_ancestor(fname)
+          if root == nil then
+            root = util.path.dirname(fname)
+          end
+          return root
+        end,
+      },
     },
 
     {
