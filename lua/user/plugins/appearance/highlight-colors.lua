ee684cdd94e9d0905b3bb3a0fd938476b17382aa
nazo6 <git@nazo6.dev>
2024-11-22 14:38:56 +0900

    disable highlight-colors

--- lua/user/plugins/appearance/highlight-colors.lua
+++ lua/user/plugins/appearance/highlight-colors.lua
@@ -2,6 +2,8 @@ return {
   "brenoprata10/nvim-highlight-colors",
   cmd = { "HighlightColors" },
   event = { "BufRead" },
+  -- I found out that tinymist crashes with documentColor lsp request, so temporarily disabled
+  enabled = false,
   config = function()
     require("nvim-highlight-colors").setup {
       render = "virtual",
