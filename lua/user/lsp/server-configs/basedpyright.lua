6e352b5312ba4b5dfee8c2b1214458bad9a511b1
nazo6 <git@nazo6.dev>
2025-05-07 13:52:52 +0900

    priorize venv in basedpyright

--- lua/user/lsp/server-configs/basedpyright.lua
+++ lua/user/lsp/server-configs/basedpyright.lua
@@ -1,3 +1,13 @@
 local create_setup = require("user.lsp.config-builder").create_setup
 
-return create_setup {}
+return create_setup {
+  root_dir = function(bufnr, cb)
+    local venv = vim.fs.root(bufnr, { ".venv" })
+    if venv ~= nil then
+      cb(venv)
+      return
+    end
+
+    cb(vim.fs.root(bufnr, { "pyproject.toml", "requirements.txt" }))
+  end,
+}
