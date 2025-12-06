dbc44b70897b114cba72a63d063f08028985aa9d
nazo6 <git@nazo6.dev>
2025-12-06 15:53:39 +0900

    cleanup toggleterm

--- lua/user/plugins/tools/toggleterm.lua
+++ lua/user/plugins/tools/toggleterm.lua
@@ -1,28 +1,3 @@
-_G.__toggleterm_lazy_cmds = {}
-local function create_toggle_lazy(cmd)
-  return function()
-    if __toggleterm_lazy_cmds[cmd] == nil then
-      __toggleterm_lazy_cmds[cmd] = require("toggleterm.terminal").Terminal:new {
-        cmd = cmd,
-        direction = "float",
-        hidden = true,
-        start_in_insert = true,
-        on_create = function(term)
-          vim.api.nvim_set_option_value("scrolloff", 0, { win = term.window })
-          vim.api.nvim_set_option_value("sidescrolloff", 0, { win = term.window })
-        end,
-        on_exit = function(t)
-          vim.api.nvim_buf_delete(t.bufnr, { force = true })
-          _G.term_lazygit = nil
-        end,
-      }
-    end
-    __toggleterm_lazy_cmds[cmd]:toggle()
-  end
-end
-
-local toggle_gitui = create_toggle_lazy "gitui"
-
 return {
   "akinsho/toggleterm.nvim",
 
@@ -39,14 +14,9 @@ return {
     map("t", [[<c-\>]], "<cmd>ToggleTerm<cr>", { desc = "[toggleterm] toggle" })
     map("t", "<ESC>", "<C-\\><C-n>")
 
-    map("n", "<C-g><C-u>", function()
-      toggle_gitui()
-    end, { desc = "Toggle gitui" })
-
-    local termau = vim.api.nvim_create_augroup("terminal", { clear = true })
     vim.api.nvim_create_autocmd("TermOpen", {
       pattern = "*",
-      group = termau,
+      group = vim.api.nvim_create_augroup("terminal", { clear = true }),
       callback = function()
         map("n", "q", [[<cmd>q<CR>]], { buffer = 0, desc = "Quit terminal buffer" })
       end,
