5937bf35813d838fbe3cb99eb48e9b583a6e7e7e
nazo6 <git@nazo6.dev>
2026-02-17 21:06:31 +0900

    fix rust-analyzer diagnostics

--- lua/user/lsp/server-configs/rust_analyzer.lua
+++ lua/user/lsp/server-configs/rust_analyzer.lua
@@ -8,4 +8,10 @@ return create_setup {
       },
     },
   },
+  on_init = function(client)
+    -- FIXME:
+    -- Disables "pull" diagnostics, which causes problems with rust-analyzer.
+    -- Related?: https://github.com/neovim/neovim/issues/29927
+    client.server_capabilities.diagnosticProvider = nil
+  end,
 }
